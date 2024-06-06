b833ee408f81b008b0b7919a38b1014016761a1e
Pedro Carvalho <pedrocarvalho3232@hotmail.com>
2024-06-06 12:49:59 -0300

    portfolio2

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// menu-hamburguer
+const menuHamburguer = document.querySelector('.menu-hamburguer')
+menuHamburguer.addEventListener('click', () =>{
+    toggleMenu()
+});
+
+function toggleMenu(){
+    const nav = document.querySelector ('.nav-responsivo');
+    menuHamburguer.classList.toggle('charge');
+
+    if(menuHamburguer.classList.contains('charge')){
+        nav.style.display = 'block';
+    } else{
+        nav.style.display = 'none'
+    }
+}
+
+document.getElementById('read-more-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    var moreText = document.getElementById('more-text');
+    var readMoreBtn = document.getElementById('read-more-btn');
+
+    if (moreText.style.display === 'none') {
+        moreText.style.display = 'block';
+        readMoreBtn.textContent = 'Read Less';
+    } else {
+        moreText.style.display = 'none';
+        readMoreBtn.textContent = 'Read More';
+    }
+});
